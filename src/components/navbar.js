a2317406d394365fa6d29b05ad94c62e941f4a54
Don Kats <donnykats@gmail.com>
2020-08-31 09:20:12 +0200

    fix: added background img after bulma import

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,8 @@ function Navbar() {
 
   return (
     <div>
-
+      <section class="hero is-primary header-image">
+        <div class="hero-head">
           <header class="navbar">
             <nav className="navbar" role="navigation" aria-label="main navigation">
               <div className="navbar-brand">
@@ -66,7 +67,8 @@ function Navbar() {
           </header>
         </div>
 
-
+      </section>
+    </div>
   )
 }
 
